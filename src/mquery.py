855f093b28afdc048a8fa7ac8f1c8c8a8aacb7f4
Jared Stroud <1250113+jaredestroud@users.noreply.github.com>
2019-10-02 21:31:34 -0400

    Main query application

---
+++ src/mquery.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from libs import libquery
+import argparse
+
+if __name__ == "__main__":
+
+    print("===[> MalQuery <]===")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--provider", help="Specify provider \
+            Malshare, Malwr)", required=False, default="all")
+
+    parser.add_argument("--hash", help="Specify hash (MD5, SHA128, SHA256)",
+            required=False)
+
+    parser.add_argument("--action", help="(download, lookup, api_info)",
+            required=False)
+
+    args = parser.parse_args()
+    query = MalQuery(args.provider, args.action, args.action)
