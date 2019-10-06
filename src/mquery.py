196851c7f49d311fdfe6f1a4a1f3a2c4513f5ba6
Jared Stroud <1250113+jaredestroud@users.noreply.github.com>
2019-10-06 16:06:13 -0400

    Malwr doesn't currently exist

--- src/mquery.py
+++ src/mquery.py
@@ -3,12 +3,11 @@ from libs.libquery import MalQuery
 import argparse
 
 if __name__ == "__main__":
-
-    print("===[> MQuery <]===")
+    print("[================[>MQuery<==================]")
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--provider", help="Specify provider \
-            Malshare, Malwr)", required=False, default="all")
+            malshare, hba)", required=False, default="all")
 
     parser.add_argument("--hash", help="Specify hash (MD5, SHA128, SHA256)",
             required=False)
