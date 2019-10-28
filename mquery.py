f107da9f95cd488fef9aac9116cc91053b3dd239
Jared Stroud <1250113+jaredestroud@users.noreply.github.com>
2019-10-28 17:34:21 -0400

    PEP8/Pylint fixes

--- mquery.py
+++ mquery.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
+'''
+MQuery CLI Utility
+'''
 from providers.libquery import MalQuery
 import sys
 import argparse
 
 if __name__ == "__main__":
+
     print("[================[ >MQuery< ]==================]\n")
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--provider", help="Specify provider (malshare, hba, " \
-            "vt, caesar).",choices=['caesar', 'virustotal', 'malshare', 'hba'],
-            required=False, default="all")
+                        "vt, caesar).", choices=['caesar', 'virustotal', 'malshare', 'hba'],
+                        required=False, default="all")
 
     parser.add_argument("--hash", help="Specify hash (MD5, SHA128, SHA256).",
-            required=False)
+                        required=False)
 
-    parser.add_argument("--action", choices=['download','search','list','info'], 
-            help="specify request type.", required=True)
+    parser.add_argument("--action", choices=['download', 'search', 'list', 'info'],
+                        help="specify request type.", required=True)
 
     args = parser.parse_args()
+
     if (args.action == "search" or args.action == "download") and args.hash is None:
         print("\t[!] Hash not specified!\n")
         sys.exit(1)
